e034d2848c11b07ce568618183df3fae6017375a
LiminalCrab <devpreston@protonmail.com>
2021-02-18 23:29:07 -0500

    added ListCreate and ListContainer components

---
+++ src/components/ListContainer.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+
+import sites from "./Site.vue"
+import createsite from "./CreateSite.vue"
+import sitelist from "./SiteList.vue"
+import listcreate from "./ListCreate"
+
+export default {
+    name: "list-create",
+    props: {
+
+    },
+    methods: {
+
+    },
+    components: { sitelist, createsite, sites, listcreate}  
+};
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ListCreate.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+
+import sites from "./Site.vue"
+import createsite from "./CreateSite.vue"
+import sitelist from "./SiteList.vue"
+
+export default {
+    name: "list-create",
+    props: {
+
+    },
+    methods: {
+
+    },
+    components: { sitelist, createsite, sites}  
+};
+
+</script>
+
+<style scoped>
+
+</style>
