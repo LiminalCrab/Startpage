dc7e92b0b6006cb5709628f8175036fc3a9aa832
LiminalCrab <devpreston@protonmail.com>
2021-02-20 16:15:18 -0500

    Cleaning up.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import moment from 'moment'
 
 Vue.config.productionTip = false
-Vue.prototype.$moment = moment;
 
 new Vue({
   render: h => h(App),
